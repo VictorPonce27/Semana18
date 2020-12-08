ec9931718c29541193ffd791e4a733bc9559268e
KharrlozGM <67246494+KharrlozGM@users.noreply.github.com>
2020-12-08 11:02:27 -0800

    Update Trivia.h

---
+++ Trivia.h
@@ -0,0 +1,15 @@
+class Trivia{
+private:
+    string info;
+public:
+    Trivia();
+    Trivia(string infoNueva);
+}
+
+Trivia::Trivia(){
+    info = "default";
+}
+
+Trivia::Trivia(infoNueva){
+    info = infoNueva;
+}
